7e00d8a95c475d5b4a8d51b9333cb41547826d3c
umayer10-AI <mdumayer10@gmail.com>
2026-05-05 09:34:57 +0600

    add

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req,res) => {
     res.end("Hello Home Page Umayer")
 })
 
+const users = [
+    {id: 1, name: "Jhon Doe", email: "jhon@example.com"},
+    {id: 2, name: "Jhon Doe", email: "jhon@example.com"},
+    {id: 3, name: "Jhon Doe", email: "jhon@example.com"},
+]
+
+app.get('/user', (req,res) => {
+    res.end(users)
+})
+
 app.listen(port, () => {
     console.log(`running on ${port}`)
 })
