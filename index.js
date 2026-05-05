7cefb3364c1b45d6291eaed77b4d87bf51cea036
umayer10-AI <mdumayer10@gmail.com>
2026-05-05 08:27:08 +0600

    add

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (req,res) => {
+    res.end("Hello Home Page")
+})
+
+app.listen(port, () => {
+    console.log(`running on ${port}`)
+})
